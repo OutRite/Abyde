14951bbfee529967af3cbdb7ecbb985af5f7c0e2
OutRite <60903237+OutRite@users.noreply.github.com>
2020-07-17 13:51:14 -0700

    REFORMATTING

--- compilers/compiler.py
+++ compilers/compiler.py
@@ -31,51 +31,52 @@ def abyde_print(text):
 
 def abyde_multiply(num, times, outvar):
 	output_prg = '||'
-	output_prg += 'm ro {}|'.format(num)
-	output_prg += 'm rt ro|'
-	output_prg += 'm ra {}||'.format(int(times)-1)
-	output_prg += 'b ra|'
-	output_prg += 'a ro rt|'
-	output_prg += 'm rb ro|'
-	output_prg += 's ra 1|'
-	output_prg += 'm ra ro|'
-	output_prg += 'm ro rb|'
-	output_prg += 'r||'
-	output_prg += 'm {} ro|'.format(outvar)
+	output_prg += 'm ro {}|'.format(num) # we move the number into ro
+	output_prg += 'm rt ro|' # we move ro into rt
+	output_prg += 'm ra {}||'.format(int(times)-1) # we move the iteration count into ra
+	output_prg += 'b ra|' # we check if we've gone through all iterations
+	output_prg += 'a ro rt|' # we haven't, so we add ro and the first number
+	output_prg += 'm rb ro|' # we move ro into rb
+	output_prg += 's ra 1|' # we decrease the iteration count
+	output_prg += 'm ra ro|' # we move everything back into normal positions
+	output_prg += 'm ro rb|' # and now ro contains our new number
+	output_prg += 'r||' # repeat until done
+	output_prg += 'm {} ro|'.format(outvar) # we move our new number into whatever variable provided
 	return output_prg
 
 
 
 for i in range(len(commands)):
-	if commands[i].split(' ')[0] == 'print':
+	current_command = commands[i].split(' ')
+	if current_command[0] == 'print':
 		compiled_prg += abyde_print(commands[i][6:])
-	elif commands[i].split(' ')[0] == 'exit':
+	elif current_command[0] == 'exit':
 		compiled_prg += 'q|'
-	elif commands[i].split(' ')[0] == 'add':
+	elif current_command[0] == 'add':
 		compiled_prg += 'a '
-		compiled_prg += commands[i].split(' ')[1]
+		compiled_prg += current_command[1]
 		compiled_prg += ' '
-		compiled_prg += commands[i].split(' ')[3]
+		compiled_prg += current_command[3]
 		compiled_prg += '|'
-		compiled_prg += 'm {} ro|'.format(commands[i].split(' ')[5]) 
-	elif commands[i].split(' ')[0] == 'sub':
+		compiled_prg += 'm {} ro|'.format(current_command[5]) 
+	elif current_command[0] == 'sub':
 		compiled_prg += 's '
-		compiled_prg += commands[i].split(' ')[1]
+		compiled_prg += current_command[1]
 		compiled_prg += ' '
-		compiled_prg += commands[i].split(' ')[3]
-		compiled_prg += '|m {} ro|'.format(commands[i].split(' ')[5])
-	elif commands[i].split(' ')[0] == 'display':
-		compiled_prg += 'm rs {}|o|'.format(commands[i].split(' ')[1])
-	elif commands[i].split(' ')[0] == 'notzero':
-		compiled_prg += 'b {}|'.format(commands[i].split(' ')[1])
-	elif commands[i].split(' ')[0] == 'endif':
+		compiled_prg += current_command[3]
+		compiled_prg += '|m {} ro|'.format(current_command[5])
+	elif current_command[0] == 'display':
+		compiled_prg += 'm rs {}|o|'.format(current_command[1])
+	elif current_command[0] == 'notzero':
+		compiled_prg += 'b {}|'.format(current_command[1])
+	elif current_command[0] == 'endif':
 		compiled_prg += '||'
-	elif commands[i].split(' ')[0] == 'set':
-		compiled_prg += 'm {} {}|'.format(commands[i].split(' ')[1], commands[i].split(' ')[3])
-	elif commands[i].split(' ')[0] == 'input':
-		compiled_prg += 'i|m {} ro|'.format(commands[i].split(' ')[2])
-	elif commands[i].split(' ')[0] == 'multiply':
-		compiled_prg += abyde_multiply(commands[i].split(' ')[1], commands[i].split(' ')[3], commands[i].split(' ')[5])
+	elif current_command[0] == 'set':
+		compiled_prg += 'm {} {}|'.format(current_command[1], current_command[3])
+	elif current_command[0] == 'input':
+		compiled_prg += 'i|m {} ro|'.format(current_command[2])
+	elif current_command[0] == 'multiply':
+		compiled_prg += abyde_multiply(current_command[1], current_command[3], current_command[5])
 	else:
 		print("ERROR: INVALID COMMAND")
 		print(commands[i])
