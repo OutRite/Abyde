638ba2278d4b6fdcfafa4d581874cba0d68ac735
OutRite <60903237+OutRite@users.noreply.github.com>
2020-07-16 19:57:50 -0700

    quick patch part 2 electric patchaloo

--- compilers/compiler.py
+++ compilers/compiler.py
@@ -43,7 +43,7 @@ for i in range(len(commands)):
 		compiled_prg += ' '
 		compiled_prg += commands[i].split(' ')[3]
 		compiled_prg += '|'
-		compiled_prg += 'm ro {}|'.format(commansd[i].split(' ')[5]) 
+		compiled_prg += 'm ro {}|'.format(commands[i].split(' ')[5]) 
 	elif commands[i].split(' ')[0] == 'display':
 		compiled_prg += 'm rs {}|o|'.format(commands[i].split(' ')[1])
 	else:
