f148e5b16696affa1f83fd1a36aa26a296aa4538
OutRite <60903237+OutRite@users.noreply.github.com>
2020-07-16 19:49:28 -0700

    bare minimum compiler

---
+++ compilers/compiler.py
@@ -0,0 +1,46 @@
+#AbydeScript to Abyde compiler.
+import sys
+
+if len(sys.argv) < 2:
+	print("Usage: ./compiler.py [program.as]")
+	sys.exit()
+
+as_file = open(sys.argv[1])
+
+as_prg = as_file.read()
+
+as_file.close()
+
+as_prg = as_prg.replace('\r', '') #augh windows i hate you
+
+commands = as_prg.split('\n')
+
+compiled_prg = ''
+
+def abyde_print(text):
+	text_ints = list(text)
+	for i in range(len(text_ints)):
+		if text_ints[i] == '`':
+			text_ints[i] = ord('\n')
+		else:
+			text_ints[i] = ord(text_ints[i])
+	output_prg = ''
+	for j in range(len(text_ints)):
+		output_prg += 'm rs {}|o|'.format(text_ints[j])
+	return output_prg
+
+
+
+
+for i in range(len(commands)):
+	if commands[i].split(' ')[0] == 'print':
+		compiled_prg += abyde_print(commands[i][6:])
+	elif commands[i].split(' ')[0] == 'exit':
+		compiled_prg += 'q|'
+	else:
+		print("ERROR: INVALID COMMAND")
+		print(commands[i])
+
+compiled_prg += 'q'
+
+print(compiled_prg)
